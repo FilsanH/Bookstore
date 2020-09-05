68ddb0031b7dfa2c438def9bbd72d95649b59d3e
Filsan Hassan <fh363.cam.ac.uk>
2020-09-05 12:05:40 +0100

    update

--- routes/authors.js
+++ routes/authors.js
@@ -2,18 +2,43 @@ const express = require('express')
 const Author = require('../models/author')
 const router = express.Router()
 
-// ALL Authors Route 
+// ALL Authors Route
 router.get('/', (req, res) => {
   res.render('authors/index')
 }) // root of application
 
-//New Author Route 
+//New Author Route
 router.get('/new', (req, res) => {
-  res.render('authors/new', {author: new Author()})
+  res.render('authors/new', { author: new Author() })
 }) // root of application
 
-//Create Author Route 
-router.post('/', (req,res)=>{
- res.send('create')
+//Create Author Route
+router.post('/', (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  })
+  console.log(author)
+  try{
+const newAuthor = await author.save()
+   // res.redirect(`authours/${newAuthor.id}`)
+      res.redirect('authors')
+  } catch{
+         res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error creating Author',
+      })   
+  }
+
+  // author.save((err, newAuthor) => {
+  //   if (err) {
+  //     res.render('authors/new', {
+  //       author: author,
+  //       errorMessage: 'Error creating Author',
+  //     })
+  //   } else {
+  //     // res.redirect(`authours/${newAuthor.id}`)
+  //     res.redirect('authors')
+  //   }
+  // })
 })
 module.exports = router
